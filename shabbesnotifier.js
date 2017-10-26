008059ec14aaa802e39747d9bc59fc7a921745b0
Shabbes Notifier <shabbes_notifier@protonmail.com>
2017-10-26 22:41:47 +0200

    Remove dead code.

--- shabbesnotifier.js
+++ shabbesnotifier.js
@@ -33,31 +33,5 @@ function documentBlocks(document)
 consoleLog("subscribing");
 browser.runtime.onMessage.addListener(shabbesOnMessage);
 
-/*
-consoleLog("link count is " + document.links.length);
-for (var i=0; i < document.links.length; i++)
-{
-  consoleLog("link[" + i + "] is " + document.links[i]);
-}
- */
-
-/*
-consoleLog("scripts count is " + document.scripts.length);
-for (var i=0; i < document.scripts.length; i++)
-{
-  var script = document.scripts[i];
-  // formalize: script.src contains "hollyclock.com"
-  if (script.src)
-  {
-    var pattern = /holyclock.com/i;
-    var count = script.src.search(pattern);
-    if (count > 0)
-    {
-      console.log("Shabbes Notifier: site contains " + count + " references to holyclock.com");
-    }
-  }
-}
- */
- 
 documentBlocks(document);
 
